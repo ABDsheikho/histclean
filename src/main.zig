c3298c4b2007150a9c29cf654669df8f5381b638
Abdulrahman Sheikho <abdulrahman.j.sheikho@gmail.com>
2026-06-15 15:37:47 +0300

    remove unnecessary code

--- src/main.zig
+++ src/main.zig
@@ -80,36 +80,6 @@ pub fn main(init: std.process.Init) !void {
     }
 
     try writer.flush();
-
-    // var file_reader = histfile.reader(init.io, &.{});
-    // const reader = &file_reader.interface;
-    // const bytes_read = try reader.read(&content);
-
-    // // Prints to stderr, unbuffered, ignoring potential errors.
-    // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    //
-    // // This is appropriate for anything that lives as long as the process.
-    // const arena: mem.Allocator = init.arena.allocator();
-    //
-    // // Accessing command line arguments:
-    // const args = try init.minimal.args.toSlice(arena);
-    // for (args) |arg| {
-    //     std.log.info("arg: {s}", .{arg});
-    // }
-    //
-    // // In order to do I/O operations need an `Io` instance.
-    // const io = init.io;
-    //
-    // // Stdout is for the actual output of your application, for example if you
-    // // are implementing gzip, then only the compressed bytes should be sent to
-    // // stdout, not any debugging messages.
-    // var stdout_buffer: [1024]u8 = undefined;
-    // var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    // const stdout_writer = &stdout_file_writer.interface;
-    //
-    // try history_cleaner.printAnotherMessage(stdout_writer);
-    //
-    // try stdout_writer.flush(); // Don't forget to flush!
 }
 
 fn printHelp() void {
