1b8ebebeac550fcc6d0bd27154548ff766e1237e
Abdulrahman Sheikho <abdulrahman.j.sheikho@gmail.com>
2026-06-21 12:24:30 +0300

    cleanup, renames, and refactor

--- src/main.zig
+++ src/main.zig
@@ -15,71 +15,51 @@ const Args = struct {
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
     const arena: mem.Allocator = init.arena.allocator();
-
-    // Accessing command line arguments:
-    var args = try init.minimal.args.iterateAllocator(arena);
-    defer args.deinit();
-
     const env = init.environ_map;
 
-    _ = args.next(); // discard binary name
-    var arg_struct = Args{};
-    while (args.next()) |arg| {
-        if (mem.eql(u8, arg, "-h") or mem.eql(u8, arg, "--help")) arg_struct.help = true;
-        if (mem.eql(u8, arg, "-d") or mem.eql(u8, arg, "--dry-run")) arg_struct.dryRun = true;
-        if (mem.eql(u8, arg, "-b") or mem.eql(u8, arg, "--backup")) arg_struct.backup = true;
-        if (mem.eql(u8, arg, "-i") or mem.eql(u8, arg, "--input")) assignPath(&arg_struct.input_path, &args);
-        if (mem.eql(u8, arg, "-o") or mem.eql(u8, arg, "--output")) assignPath(&arg_struct.output_path, &args);
-    }
+    const args = try parseArgs(init.minimal.args, arena);
 
-    if (arg_struct.help) return printHelp();
+    if (args.help) return printHelp(); // print help and exit
 
-    const histfile_path = try getHistoryPath(arg_struct.input_path, env, arena);
+    const histfile_path = try getHistoryPath(args.input_path, env, arena);
 
     const histfile = try Io.Dir.openFile(Io.Dir.cwd(), io, histfile_path, .{ .mode = .read_write });
     errdefer histfile.close(io);
 
     // backup history file before proceeding
-    if (arg_struct.backup and !arg_struct.dryRun) try backupFile(histfile_path, io);
+    if (args.backup and !args.dryRun) try backupFile(histfile_path, io);
 
+    // read file into memory
     const file_stat = try histfile.stat(io);
-
     const content = try arena.alloc(u8, file_stat.size);
     defer arena.free(content);
     _ = try histfile.readPositionalAll(io, content, 0);
 
-    var backward_lines = mem.splitBackwardsAny(u8, content, "\n\r");
-
-    var hset: std.StringHashMap(void) = .init(arena);
-
-    var new_lines: std.ArrayList([]const u8) = .empty;
+    var new_lines = try filterLines(content, arena);
     defer new_lines.deinit(arena);
 
-    var time_stamp_flag = false;
-    while (backward_lines.next()) |line| {
-        const clean_line = mem.trim(u8, line, " ");
-        if (hset.contains(clean_line)) continue;
-
-        if (mem.startsWith(u8, clean_line, "#")) {
-            if (time_stamp_flag) continue else time_stamp_flag = true;
-        } else time_stamp_flag = false;
-
-        try hset.put(clean_line, {});
-        try new_lines.insert(arena, 0, clean_line);
-    }
-
-    const output_file = try getOutputFile(arg_struct, io, &histfile);
+    const output_file = try openOutputFile(args, io, &histfile);
     defer output_file.close(io);
 
     var result_writer = output_file.writer(io, &.{});
-    const writer = &result_writer.interface;
+    try writeLines(&result_writer.interface, new_lines);
+}
 
-    _ = try writer.print("{s}", .{new_lines.items[0]});
-    for (new_lines.items[1..]) |item| {
-        _ = try writer.print("\n{s}", .{item});
+fn parseArgs(args: std.process.Args, allocator: mem.Allocator) !Args {
+    var args_iter = try args.iterateAllocator(allocator);
+    defer args_iter.deinit();
+
+    _ = args_iter.next(); // discard binary name
+
+    var arg_struct = Args{};
+    while (args_iter.next()) |arg| {
+        if (mem.eql(u8, arg, "-h") or mem.eql(u8, arg, "--help")) arg_struct.help = true;
+        if (mem.eql(u8, arg, "-d") or mem.eql(u8, arg, "--dry-run")) arg_struct.dryRun = true;
+        if (mem.eql(u8, arg, "-b") or mem.eql(u8, arg, "--backup")) arg_struct.backup = true;
+        if (mem.eql(u8, arg, "-i") or mem.eql(u8, arg, "--input")) assignPath(&arg_struct.input_path, &args_iter);
+        if (mem.eql(u8, arg, "-o") or mem.eql(u8, arg, "--output")) assignPath(&arg_struct.output_path, &args_iter);
     }
-
-    try writer.flush();
+    return arg_struct;
 }
 
 fn printHelp() void {
@@ -108,8 +88,8 @@ fn printHelp() void {
     std.debug.print("{s}", .{msg});
 }
 
-fn assignPath(str: *?[]const u8, args: *std.process.Args.Iterator) void {
-    if (args.next()) |path| check: {
+fn assignPath(str: *?[]const u8, args_iter: *std.process.Args.Iterator) void {
+    if (args_iter.next()) |path| check: {
         if (mem.startsWith(u8, path, "-")) break :check;
         str.* = path;
         return;
@@ -120,6 +100,7 @@ fn assignPath(str: *?[]const u8, args: *std.process.Args.Iterator) void {
         \\           ex: histclean -i -d     -> error
         \\       Or does the file-path start with a hyphen (-)?
         \\           ex: histclean -i -/path/to/error
+        \\
         \\
     ;
     std.debug.print(error_msg, .{});
@@ -137,10 +118,10 @@ fn getHistoryPath(file_path: ?[]const u8, env: *std.process.Environ.Map, allocat
     if (env.get("HOME")) |home| {
         return try Io.Dir.path.join(allocator, &[_][]const u8{ home, "history" });
     }
-    return error.HistorFileNotFound;
+    return error.HistoryFileNotFound;
 }
 
-fn getOutputFile(args: Args, io: Io, defaultFile: *const Io.File) !Io.File {
+fn openOutputFile(args: Args, io: Io, defaultFile: *const Io.File) !Io.File {
     if (args.dryRun) {
         return Io.File.stdout();
     }
@@ -156,6 +137,47 @@ fn backupFile(path: []const u8, io: Io) !void {
     const cwd = Io.Dir.cwd();
     const dest_path = try std.fmt.bufPrint(&buffer, "{s}.backup", .{path});
     try Io.Dir.copyFile(cwd, path, cwd, dest_path, io, .{ .replace = true });
+}
+
+fn filterLines(content: []u8, allocator: mem.Allocator) !std.ArrayList([]const u8) {
+    // define lines as array of strings
+    var lines: std.ArrayList([]const u8) = .empty;
+
+    // Read content from end to start
+    var backward_lines = mem.splitBackwardsAny(u8, content, "\n\r");
+
+    // define a set to keep record of unique lines
+    var hash_set: std.StringHashMap(void) = .init(allocator);
+    defer hash_set.deinit();
+
+    // define a flag to prevent consecutive time-stamps (empty command)
+    var time_stamp_flag = false;
+
+    while (backward_lines.next()) |line| {
+        const clean_line = mem.trim(u8, line, " ");
+        if (hash_set.contains(clean_line)) continue;
+
+        // for consecutive time-stamps, keep the first, skip the rest
+        if (mem.startsWith(u8, clean_line, "#")) {
+            if (time_stamp_flag) continue else time_stamp_flag = true;
+        } else time_stamp_flag = false;
+
+        try hash_set.put(clean_line, {});
+        try lines.insert(allocator, 0, clean_line);
+    }
+    return lines;
+}
+
+fn writeLines(writer: *Io.Writer, lines: std.ArrayList([]const u8)) !void {
+    // Write first line without \n newline char
+    // The next lines start with \n newline char
+    // This prevent getting empty line at the end of a file
+    try writer.print("{s}", .{lines.items[0]});
+    for (lines.items[1..]) |item| {
+        try writer.print("\n{s}", .{item});
+    }
+
+    try writer.flush();
 }
 
 test "simple test" {
